8b75cf9a3ca5e1dde1af760f32af137c92f8c431
Pasha <amberswcx@gmail.com>
2021-11-21 11:40:49 +0400

    feat: complete 1 kata (8 kyu)

---
+++ codewars/Even or Odd.js
@@ -0,0 +1,11 @@
+/*// 1st decision
+
+function even_or_odd(number) {
+  return (number % 2 === 0) ? 'Even' : 'Odd';
+}*/
+
+// 2nd decision
+
+function even_or_odd(number) {
+  return (number % 2) ? 'Odd' : 'Even';
+}
