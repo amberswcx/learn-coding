560bc786ad013a42103a964f87eba3cd159153ac
Pasha <amberswcx@gmail.com>
2021-12-12 20:05:52 +0400

    feat: complete 1 task (1st part page 190/755)

---
+++ learn-js/page190-task1.js
@@ -0,0 +1,25 @@
+// not my solution
+
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep: function() {
+    console.log(this.step);
+    return this;
+  }
+};
+
+ladder
+  .up()
+  .up()
+  .down()
+  .up()
+  .down()
+  .showStep();
