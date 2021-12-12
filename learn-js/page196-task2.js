e31d70fcab7f5c81b0fe66cf525eccfd56edf387
Pasha <amberswcx@gmail.com>
2021-12-12 20:50:39 +0400

    feat: complete 1 task (1st part page 196/755)

---
+++ learn-js/page196-task2.js
@@ -0,0 +1,12 @@
+function Accumulator(startingValue) {
+  this.value = startingValue;
+
+  this.read = function() {
+    this.value += +prompt('what value?', 0);
+  }
+}
+
+let accumulator = new Accumulator(1); // начальное значение 1
+accumulator.read(); // прибавит ввод prompt к текущему значению
+accumulator.read(); // прибавит ввод prompt к текущему значению
+alert(accumulator.value); // выведет сумму этих значений
