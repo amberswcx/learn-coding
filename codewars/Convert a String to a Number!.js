14987592699b1c2f05ca5f789ead9ae43a462764
Pasha <amberswcx@gmail.com>
2021-12-13 05:42:58 +0400

    feat: complete 1 kata (8 kyu)

---
+++ codewars/Convert a String to a Number!.js
@@ -0,0 +1,14 @@
+/*
+// 1st solution
+var stringToNumber = function(str) {
+  return +str;
+}
+*/
+
+/*
+// 2nd solution
+const stringToNumber = str => Number(str);
+*/
+
+// 3rd solution
+const stringToNumber = str => parseInt(str);
