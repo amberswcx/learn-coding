47e9df5568082e26fd0295cc2441ed85314c9d95
Pasha <amberswcx@gmail.com>
2022-01-01 16:07:26 +0400

    feat: complete 1 task (page 333/755)

---
+++ learn-js/page333-task1.js
@@ -0,0 +1,26 @@
+let room = {
+  number: 23
+};
+
+let meetup = {
+  title: "Совещание", occupiedBy: [{name: "Иванов"}, {name: "Петров"}], place: room
+};
+
+// цикличные ссылки
+room.occupiedBy = meetup;
+meetup.self = meetup;
+
+console.log(JSON.stringify(meetup, function replacer(key, value) {
+  return (key !== '' && value === meetup) ? undefined : value;
+  // нужно проверить key =="" , чтобы исключить первый вызов,
+  // где значение value равно meetup
+}));
+
+/* в результате должно быть:
+{
+  "title":"Совещание",
+  "occupiedBy":[{"name":"Иванов"},{"name":"Петров"}],
+  "place":{"number":23}
+}
+*/
+
