276dddf740239c46bbc5b2e3b128b050045f8a91
Pasha <amberswcx@gmail.com>
2022-02-12 08:25:16 +0400

    feat: complete sql 1.2 step 5

---
+++ stepik-sql/1-2-step5.sql
@@ -0,0 +1,28 @@
+CREATE TABLE book(
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(50),
+    author VARCHAR(30),
+    price DECIMAL(8, 2),
+    amount INT
+);
+
+INSERT INTO book(title, author, price, amount)
+VALUES ('Мастер и Маргарита', 'Булгаков М.А.', 670.99, 3);
+
+INSERT INTO book(title, author, price, amount)
+VALUES ('Белая гвардия', 'Булгаков М.А.', 540.50, 5);
+
+INSERT INTO book(title, author, price, amount)
+VALUES ('Идиот', 'Достоевский Ф.М.', 460.00, 10);
+
+INSERT INTO book(title, author, price, amount)
+VALUES ('Братья Карамазовы', 'Достоевский Ф.М.', 799.01, 2);
+
+INSERT INTO book(title, author, price, amount)
+VALUES ('Стихотворения и поэмы', 'Есенин С.А.', 650.00, 15);
+
+-- 1.2 Выборка данных
+
+SELECT title, amount,
+    amount * 1.65 AS pack
+FROM book;
