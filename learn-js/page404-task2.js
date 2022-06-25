691168c73f5d9d8de13b7a937a56018b8a792d5d
Pasha <amberswcx@gmail.com>
2022-06-25 21:28:37 +0400

    feat: complete  task (page 404/755)

---
+++ learn-js/page404-task2.js
@@ -0,0 +1,17 @@
+function f(x) {
+  console.log(x);
+}
+
+function delay(f, ms) {
+  function timeout(...args) {
+    setTimeout(() => f.apply(this, args), ms);
+  }
+
+  return timeout;
+}
+
+// создаём обёртки
+let f1000 = delay(f, 1000);
+let f1500 = delay(f, 1500);
+f1000('test'); // показывает "test" после 1000 мс
+f1500('test'); // показывает "test" после 1500 мс
