d735196d65582b4d1a5df3a9205cedd73be84c27
Pasha <amberswcx@gmail.com>
2022-06-26 09:55:59 +0400

    feat: complete 1 task (page 414/755)

---
+++ learn-js/page414-task1.js
@@ -0,0 +1,13 @@
+function askPassword(ok, fail) {
+  let password = 'rockstar';
+  if (password == 'rocksta1r') ok();
+  else fail();
+}
+
+let user = {
+  name: 'John',
+  login(result) {
+    console.log(this.name + (result ? ' logged in' : ' failed to log in'));
+  }
+};
+askPassword(user.login.bind(user, true), user.login.bind(user, false)); // ?
